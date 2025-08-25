c9da0993e1439a035cdfe4d5b01018aa6e7a08ed
kalyani shindalkar <shindalkarkalyani163@gmail.com>
2025-08-25 16:41:10 +0530

    import env and secure port

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // const express = require("express"); // Method 1
 import express from "express";  // Method 2
+import dotenv from "dotenv";
+dotenv.config({});
 
 const app = express();
 
-const PORT = 7000;
+const PORT = process.env.PORT || 7000;
 
 app.listen(PORT, () => {
     console.log(`server listen at port ${PORT}`);
-})
+});
