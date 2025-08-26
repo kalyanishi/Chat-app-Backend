08ce650086cd1efbc8cfb71b19293e12614675ec
kalyani shindalkar <shindalkarkalyani163@gmail.com>
2025-08-26 08:43:50 +0530

    Data base is connected

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // const express = require("express"); // Method 1
 import express from "express";  // Method 2
 import dotenv from "dotenv";
+import connectDB from "./config/Database.js";
+
 dotenv.config({});
+
 
 const app = express();
 
 const PORT = process.env.PORT || 7000;
 
 app.listen(PORT, () => {
+    connectDB();
     console.log(`server listen at port ${PORT}`);
 });
