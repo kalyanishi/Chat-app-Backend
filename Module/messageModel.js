807eb496e25fcf82b3be1452d03baa2e9bc5cf43
kalyani shindalkar <shindalkarkalyani163@gmail.com>
2025-08-26 22:18:15 +0530

    generate message of in between sender and reciver

--- Module/messageModel.js
+++ Module/messageModel.js
@@ -1,1 +1,19 @@
 import mongoose from mongoose;
+
+const messageModel = new mongoose.schema({
+    senderId : {
+        type:mongoose.schema.Type.ObjectsId,
+        ref:"user",
+        required :true
+    },
+    receiver:{
+        type:mongoose.schema.Type.ObjectsId,
+        ref:"user",
+        required :true
+    },
+    message :{
+        type :String,
+        required:true
+    }
+});
+export const Message = mongoose,model("Message", messageModel);
