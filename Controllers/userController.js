3c5a16b7988e024cc5819034b4d62bc19f39abcc
kalyani shindalkar <shindalkarkalyani163@gmail.com>
2025-08-27 23:03:05 +0530

    create register page if user is not available in DB

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -7,5 +7,15 @@ export const register = async(req, res) => {
         if(password != confirmpassword){
             return res.statues(400).json({message:"password not match"});
         }
+        const user = await user.findOne({username});
+        if(user){
+            return res.status(400).json({message:"username already exit try different way"})
+        }
+        await user.create({fullName,
+            username,
+            password,
+            profilephoto,
+            gender
+        })
     }catch(error) {}
 }
